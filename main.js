38cfddd7ce72344663ad724aba6a4dcfd5b391ca
112swde <82498343+112swde@users.noreply.github.com>
2022-04-28 11:46:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,10 +23,32 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video,modelLoaded);
+  poseNet.on("pose",gotPoses);
 }
 
+function modelLoaded()
+{
+  console.log("model loaded!")
+}
+
+function gotPoses(results)
+{
+   if(results)
+   {
+     console.log(results);
+   }
+}
 
 function draw(){
+
+  image(video,0,0,700,600);
 
  background(0); 
 
